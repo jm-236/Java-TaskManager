abb76d35d6ea991cce1f2c3c5f61892c3aeba80c
João Marcelo <159437965+jm-236@users.noreply.github.com>
2024-09-10 14:22:58 -0300

    Fix: Erro com a classe CustomUserDetailsService
    A classe não estava sendo inicializada corretamente pelo Spring pois não estava marcada com a tag @Service. Agora ela está marcada e funcionando corretamente.

--- taskmanager/src/main/java/edu/taskmanager/taskmanager/infra/security/CustomUserDetailsService.java
+++ taskmanager/src/main/java/edu/taskmanager/taskmanager/infra/security/CustomUserDetailsService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
@@ -13,6 +14,7 @@ import java.util.ArrayList;
  * CustomUserDetailsService class that implements UserDetailsService to provide custom user details service.
  * This class is used to load user-specific data during security authentication.
  */
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
