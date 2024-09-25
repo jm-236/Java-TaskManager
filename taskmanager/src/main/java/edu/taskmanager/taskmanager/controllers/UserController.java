1a7adc5d293cbc70e034ce7d7ef46b3f704a1f43
João Marcelo <159437965+jm-236@users.noreply.github.com>
2024-09-25 17:36:34 -0300

    Criação da classe 'UserController'
    Criação da classe userController, para testar o acesso ao endpoint padrão do usuário.

--- taskmanager/src/main/java/edu/taskmanager/taskmanager/controllers/UserController.java
+++ taskmanager/src/main/java/edu/taskmanager/taskmanager/controllers/UserController.java
@@ -1,2 +1,16 @@
-package edu.taskmanager.taskmanager.controllers;public class UserController {
+package edu.taskmanager.taskmanager.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<String> getUser() {
+        return ResponseEntity.ok("SUCESSO!");
+    }
 }
