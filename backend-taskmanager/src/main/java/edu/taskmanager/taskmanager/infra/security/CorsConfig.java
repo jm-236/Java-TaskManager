00000ec6f374cb2348f08eda9f0fa9a9b680a95c
jm-236 <jaowjjw@gmail.com>
2025-12-22 17:12:17 -0300

    fix: problemas com a versão do npm e erro de CORS.

--- backend-taskmanager/src/main/java/edu/taskmanager/taskmanager/infra/security/CorsConfig.java
+++ backend-taskmanager/src/main/java/edu/taskmanager/taskmanager/infra/security/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Aplica a configuração para todos os endpoints da API
-                .allowedOrigins("/*") // URL do seu frontend
+                .allowedOrigins("http://localhost:5173") // URL doCORS Missing Allow Origin) seu frontend
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT") // Métodos HTTP permitidos
                 .allowedHeaders("*"); // Permite todos os cabeçalhos
--- backend-taskmanager/src/test/java/edu/taskmanager/taskmanager/TaskmanagerApplicationTests.java
+++ backend-taskmanager/src/test/java/edu/taskmanager/taskmanager/TaskmanagerApplicationTests.java
@@ -1,13 +1,34 @@
 package edu.taskmanager.taskmanager;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TaskmanagerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @BeforeEach
+//    void setUp() {
+//
+//    }
+//
+//    @Test
+//    void endpointDeCadastroDeveRetornarOk() throws Exception {
+//        String content = "{\"name\": \"cacau\",\"password\": \"123\", \"email\": \"cacaugostoso@gmail.com\"}";
+//        mockMvc.perform(post("/auth/register").contentType(MediaType.APPLICATION_JSON).content(content))
+//                .andExpect(status().isOk());
+//    }
 
 }
